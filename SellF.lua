43721f571eb63ef3abce2e0f2646f17519d18ff1
Ivan Andreev <iivvaann.andreev@gmail.com>
2019-09-28 16:47:57 +0100

    Search for Osmenite Ore when opening the AH

--- SellF.lua
+++ SellF.lua
@@ -1,1 +1,36 @@
-message('Hello world')
+message("ah result")
+
+local ah_opened_event = "AUCTION_HOUSE_SHOW"
+local frame = CreateFrame("Frame")
+frame:RegisterEvent(ah_opened_event)
+frame:SetScript("OnEvent", function(self, event, ...)
+    print("AH opened")
+    local canQuery,canQueryAll = CanSendAuctionQuery()
+    if canQuery then 
+        QueryAuctionItems("Osmenite Ore", nil, nil, 0, nil, 0, false, false, nil)
+    else 
+        print("Can't query the AH")
+    end
+
+    if canQueryAll then 
+        print("Can query ALL the AH") 
+   else 
+       print("Can't query ALL the AH")
+   end
+   -- /script QueryAuctionItems("Osmen", nil, nil, 0, nil, 0, false, false, nil)
+end)
+
+-- local printResult = ""
+-- for _,v in ipairs(arg) do
+--     printResult = printResult .. tostring(v) .. "  "
+-- end
+-- print(printResult)
+
+
+
+-- API 
+
+-- QueryAuctionItems(name, minLevel, maxLevel, page,
+--   isUsable, qualityIndex, getAll, exactMatch, filterData
+-- )
+-- https://wowwiki.fandom.com/wiki/API_QueryAuctionItems
